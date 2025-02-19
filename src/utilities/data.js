a8b08e39bee6ffd5e5d6e5f3f66b7c393dbd5c4b
patrickchen8 <patrick.chen0459@gmail.com>
2025-02-18 18:27:04 -0600

    Created made up data

---
+++ src/utilities/data.js
@@ -0,0 +1,65 @@
+
+export const jobRequents = {
+
+    0 : {
+        name: "Son's Room", 
+        isIndoor: "Indoor",
+        sqft: 1000,
+        ceilingHeight: {
+            ft: 8,
+            in: 2
+        },
+        wallMaterial: "Drywall", 
+        paintType: "Glossy", 
+        numColors: 2,
+        preperationNeeded: "None",
+        wallCondition: "Clean & Smooth",
+        numCoats: 2,
+        schedule: "ASAP",
+        otherServices: "None",
+        desc: "Want to reapint my soon's room. Needs to be a shade a green because that's his favorite color. Walls are good condition",
+        img: "https://s42814.pcdn.co/wp-content/uploads/2020/01/Idea-House_Nov-2018_pg-46.0-1-scaled.jpg.optimal.jpg"
+    }, 
+
+    1 : {
+        name: "Home Office", 
+        isIndoor: "Indoor",
+        sqft: 900,
+        ceilingHeight: {
+            ft: 8,
+            in: 0
+        },
+        wallMaterial: "Drywall", 
+        paintType: "Eggshell", 
+        numColors: 1,
+        preperationNeeded: "Wall Patching",
+        wallCondition: "Minor Damage",
+        numCoats: 2,
+        schedule: "Within 1 Week",
+        otherServices: "Trim Painting",
+        desc: "The home office needs a fresh coat of eggshell paint in a single color. The walls have minor damage and require patching before painting. Additionally, the trim will be painted to match the new wall color. The job should be completed within 1 week.",
+        img: "https://st.hzcdn.com/simgs/99a1989c01d8792d_14-3218/_.jpg"
+    },
+
+    2: {
+        name: "Dining Room", 
+        isIndoor: "Indoor",
+        sqft: 1100,
+        ceilingHeight: {
+            ft: 9,
+            in: 0
+        },
+        wallMaterial: "Plaster", 
+        paintType: "Glossy", 
+        numColors: 2,
+        preperationNeeded: "Wall Cleaning",
+        wallCondition: "Clean & Smooth",
+        numCoats: 1,
+        schedule: "ASAP",
+        otherServices: "None",
+        desc: "The dining room requires a glossy paint job in two colors. The walls are clean and smooth, so only light cleaning is needed before painting. The job is urgent and should be completed as soon as possible.",
+        img: "https://www.thespruce.com/thmb/ZqfUoFjPOAqtJBpzjDw-sxRdiNQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/modern-dining-room-ideas-4147451-hero-d6333998f8b34620adfd4d99ac732586.jpg"
+    }
+
+
+}
