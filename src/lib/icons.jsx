4469ee4f974a08314b14c69fe1e916197e9c06aa
JosephJShim <josephjshim@gmail.com>
2025-02-18 19:34:17 -0600

    icons.jsx icons (pen circle)

---
+++ src/lib/icons.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const EditIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#clip0)">
+      <path
+        d="M7.14283 11.5715L8.85711 13L13.1428 7.57146M9.71425 2.14288C9.94158 1.91555 10.2499 1.78784 10.5714 1.78784C10.7306 1.78784 10.8882 1.8192 11.0353 1.88011C11.1823 1.94103 11.316 2.03032 11.4285 2.14288C11.5411 2.25544 11.6304 2.38907 11.6913 2.53614C11.7522 2.68321 11.7836 2.84084 11.7836 3.00002C11.7836 3.15921 11.7522 3.31684 11.6913 3.46391C11.6304 3.61098 11.5411 3.74461 11.4285 3.85717L4.28568 11L1.99997 11.5715L2.5714 9.28574L9.71425 2.14288Z"
+        stroke="#FFFAFA"
+        strokeWidth="1.14286"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <rect width="13.7143" height="13.7143" fill="white" transform="translate(0.285706 0.142822)" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const CircleIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+    {...props}
+  >
+    <circle cx="14" cy="14" r="12.6" fill="#2511BE" stroke="#FFFAFA" strokeWidth="1.2" />
+  </svg>
+);
