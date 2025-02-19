a3fbfcc3d74503c44b6cff06d6b23dbc048f8a5d
sungmogi <smcho1010@gmail.com>
2025-02-18 19:31:14 -0600

    swarming

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,8 +1,24 @@
+import Drawer from '@mui/material/Drawer';
+
+
 const RequestPage = () => {
 
     return (
-        <div className="text-black ">
+        <div className="flex flex-row w-full h-full bg-gray-200">
             hi
+            <Drawer
+                // sx={{
+                //     width: drawerWidth,
+                //     flexShrink: 0,
+                //     '& .MuiDrawer-paper': {
+                //         width: drawerWidth,
+                //         boxSizing: 'border-box',
+                //     },
+                // }}
+                variant="permanent"
+                anchor="left"
+                className="bg-black"
+            ></Drawer>
         </div>
     )
 }
