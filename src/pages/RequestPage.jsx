3e7f1427b8bea54f353ae4086a5952fdab5d0606
Catherine Guo <Catherineguo2025@u.northwestern.edu>
2025-02-21 15:08:30 -0600

    update info modal added

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
 import TopNavBar from '../components/TopNavBar';
 import ConfirmationModal from '../components/ConfirmationModal';
+import ConfirmationUpdateModal from '../components/ConfirmationUpdateModal';
 import { Button } from "@mui/material";
 
-
 const RequestPage = () => {
-
     const [open, setOpen] = useState(false);
-    console.log(open);
+    const [showConfirmationUpdate, setShowConfirmationUpdate] = useState(false);
+
+    const handleContractorSelect = (contractor) => {
+        setOpen(false); // Close the first modal
+        setShowConfirmationUpdate(true); // Open the confirmation update modal
+    };
 
     return (
         <div className="flex flex-col w-full h-full bg-gray-200">
-            <TopNavBar modalOpen = {open}
-                       setModalOpen={setOpen}/>
-            <ConfirmationModal />
-            {open &&
+            <TopNavBar 
+                modalOpen={open}
+                setModalOpen={setOpen}
+            />
+            
+            {/* Remove this standalone ConfirmationModal as it's duplicated */}
+            {/* <ConfirmationModal /> */}
+            
+            {open && (
                 <div style={{ textAlign: "center", marginTop: "100px" }}>
-                    <ConfirmationModal isOpen={open} onClose={() => setOpen(false)} />
+                    <ConfirmationModal 
+                        isOpen={open} 
+                        onClose={() => setOpen(false)}
+                        onContractorSelect={handleContractorSelect}
+                    />
                 </div>
-            }
+            )}
 
+            <ConfirmationUpdateModal 
+                isOpen={showConfirmationUpdate}
+                onClose={() => setShowConfirmationUpdate(false)}
+            />
         </div>
-    )
-}
+    );
+};
 
 export default RequestPage;
