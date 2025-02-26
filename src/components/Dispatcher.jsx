ee6d6d444ef40193d032ce53e70f86cc7ac35056
patrickchen8 <patrick.chen0459@gmail.com>
2025-02-25 19:31:29 -0600

    Added auth and landing page back

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -1,17 +1,29 @@
+import { createContext } from 'react';
 import { BrowserRouter, Routes, Route, Navigate  } from "react-router-dom";
-import {useEffect} from 'react';
 import RequestPage from '../pages/RequestPage';
+import LandingPage from '../pages/LandingPage'; 
 import NewProjectPage from '../pages/NewProjectPage';
+import { useAuthState } from '../utilities/firebase';
 
+
+export const userContext = createContext();
 
 const Dispatcher = () => {
     // Add user auth as needed
+    const [user, loading] = useAuthState(); 
 
-    return (
+    return user ? (
+        <userContext.Provider value = {user}>
+              <Routes>
+                <Route path="/" element={<RequestPage />} /> 
+                <Route path="/newproject" element={<NewProjectPage />} />
+                {/* Add as needed */}
+            </Routes>
+        </userContext.Provider>
+      
+    ) : (
         <Routes>
-            <Route path="/" element={<RequestPage />} /> 
-            <Route path="/newproject" element={<NewProjectPage />} /> 
-            {/* Add as needed */}
+            <Route path="*" element={<LandingPage />}/>
         </Routes>
     );
 }
