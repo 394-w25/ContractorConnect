a493df6f686779874e3e53e8171cba2900dfa2ea
patrickchen8 <patrick.chen0459@gmail.com>
2025-02-25 20:40:24 -0600

    Fixed job request context

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,23 +1,14 @@
-import React, { useState, createContext } from "react";
 import RequestInfo from '../components/RequestInfo.jsx';
-import { jobRequests } from '../utilities/data.js'
 
-
-export const jobRequestContext = createContext(); 
 
 const RequestPage = ({index, isDrawerOpen}) => {
-    const [jobReqs, setJobReqs] = useState(jobRequests); 
-
-    console.log(isDrawerOpen);
     const width = isDrawerOpen ?  "w" + (window.innerWidth - 305).toString() : "w-full";
-    console.log(width);
+  
 
     return (
-        <jobRequestContext.Provider value={{jobReqs, setJobReqs}}>
-            <div className={"flex flex-col bg-gray-200 " + width}>
-               <RequestInfo index={index} isDrawerOpen={isDrawerOpen} />
-            </div>
-        </jobRequestContext.Provider>
+        <div className={"flex flex-col bg-gray-200 " + width}>
+            <RequestInfo index={index} isDrawerOpen={isDrawerOpen} />
+        </div>
     );
 };
 
