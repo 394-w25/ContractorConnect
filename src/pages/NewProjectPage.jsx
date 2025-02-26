b5816adaaab2ceb69bc1cf01328ff96a4713bb00
Sachi Parikh <parikhsachi@gmail.com>
2025-02-25 19:33:56 -0600

    removed topnavbar, got rid of error

--- src/pages/NewProjectPage.jsx
+++ src/pages/NewProjectPage.jsx
@@ -1,15 +1,12 @@
 import React, { useState, createContext } from "react";
-import TopNavBar from '../components/TopNavBar';
-//import ChatBox from '../components/ChatBox'
 
 
 const NewProjectPage = () => {
-
     return (
-        <div className="flex flex-col w-full h-full bg-gray-200">
-            <TopNavBar />
-        </div>
+      <div>
+        <h1>New Project Page</h1>
+      </div>
     );
-};
+  };
 
 export default NewProjectPage;
