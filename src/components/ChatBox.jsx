d8dc4ecf983e4ca3c20aca9a1d63da814cfde78c
Catherine Guo <Catherineguo2025@u.northwestern.edu>
2025-02-25 19:02:30 -0600

    pages made

---
+++ src/components/ChatBox.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Modal,
+  Box,
+  Typography,
+} from "@mui/material";
+import { HomieIcon } from "../lib/icons";
+
+const ConfirmationUpdateModal = ({ isOpen, onClose }) => {
+    console.log('entered')
+  return (
+    <Modal open={isOpen} onClose={onClose} aria-labelledby="confirmation-modal">
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "white",
+          borderRadius: 3,
+          boxShadow: 24,
+          p: 4,
+          textAlign: "center",
+        }}
+      >
+        {/* Header */}
+        <Box display="flex" alignItems="center" justifyContent="center" mb={2}>
+        <HomieIcon />
+          <Typography variant="h5" fontWeight="bold">
+            Confirmed!
+          </Typography>
+        </Box>
+
+        {/* Message */}
+        <Typography variant="body1">
+          Updating your information ...
+        </Typography>
+      </Box>
+    </Modal>
+  );
+};
+
+export default ConfirmationUpdateModal;
