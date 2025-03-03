7fe5a277966f94c7f25eedc47bb4c1682d79dd36
JosephJShim <josephjshim@gmail.com>
2025-03-03 16:49:58 -0600

    unit testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,13 @@
 import {describe, expect, test} from 'vitest';
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import LandingPage from './pages/LandingPage';
 
 describe('counter tests', () => {
     
-  test("Counter should be 0 at the start", () => {
-    render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
-  });
-
-  test("Counter should increment by one when clicked", async () => {
-    render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+  test("ContractorConnect Landing Page", () => {
+    render(<LandingPage />);
+    expect(screen.getByText('Sign In')).toBeDefined();
   });
 
 });
