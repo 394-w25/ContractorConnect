d6d1011245afaa925146cb9f6799ff05f1f4d833
JosephJShim <josephjshim@gmail.com>
2025-03-05 16:28:26 -0600

    inclass testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,27 @@
 import {describe, expect, test} from 'vitest';
+import { it, vi } from 'vitest';
+
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from './utilities/firebase';
+import { useAuthState } from './utilities/firebase';
+
+
+vi.mock('../utilities/firebase');
+
 
 describe('App Component Tests', () => {
     
   test("ContractorConnect Landing Page", () => {
     render(<App />);
     expect(screen.getByText('Sign In')).toBeDefined();
+  });
+
+  test('shows Sign Out if logged in', () => {
+    const fakeUser = { displayName: 'fakeName', email: 'fakeEmail' };
+    useAuthState.mockReturnValue([fakeUser]);
+    render(<App />);
+    expect(screen.getByText(/Homie Paint Project Assistant/i)).toBeInTheDocument();
   });
 
 });
