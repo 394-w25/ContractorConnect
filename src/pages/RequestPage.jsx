269a1b790e5a264899a93deccacd1a83a951a7a2
patrickchen8 <patrick.chen0459@gmail.com>
2025-03-11 20:48:52 -0500

    Added id url param

--- src/pages/RequestPage.jsx
+++ src/pages/RequestPage.jsx
@@ -1,15 +1,22 @@
+import { createContext } from 'react';
 import RequestInfo from '../components/RequestInfo.jsx';
+import { useParams } from 'react-router-dom';
 
+
+export const idContext = createContext();
 
 const RequestPage = ({index, isDrawerOpen}) => {
+    const { id } = useParams();
+    
     const width = isDrawerOpen ?  "w" + (window.innerWidth - 305).toString() : "w-full";
     const leftMargin = isDrawerOpen ? "ml-[305px] " : "ml-0 ";
 
-
     return (
-        <div className={"flex flex-col bg-gray-200 " + leftMargin + width}>
-            <RequestInfo index={index} isDrawerOpen={isDrawerOpen} />
-        </div>
+        <idContext.Provider value={id}>
+             <div className={"flex flex-col bg-gray-200 " + leftMargin + width}>
+                <RequestInfo index={index} isDrawerOpen={isDrawerOpen} />
+            </div>
+        </idContext.Provider>
     );
 };
 
